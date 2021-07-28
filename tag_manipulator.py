16ebc55276ce2831dd23fd43a9b9ac240811062b
henbei <henrik.beil@yahoo.de>
2021-07-28 16:35:31 +0100

    cut3 more stuff

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -5,4 +5,11 @@ class TagManipulator():
         if len(tags) < 1 :
             return result
 
+        result = tags.split(",")
+        #result.append(tags)
+        for index in range(0,len(result)):
+            new_entry = result[index].strip()
+            result[index] = new_entry
+
+
         return result
