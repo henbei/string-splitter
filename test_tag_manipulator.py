731194858d2764ff42ea4197295a992e7f8d1ee7
henbei <henrik.beil@yahoo.de>
2021-07-28 16:07:10 +0100

    cut2 no real change

---
+++ test_tag_manipulator.py
@@ -0,0 +1,28 @@
+from tag_manipulator import TagManipulator    
+    
+    
+def test_split_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ""
+    expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+def test_split_comma_string_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
